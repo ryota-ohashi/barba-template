58ec4a2052201fb77c2b812e19418c09e1750c73
ryota ohashi <ryota.812.skt@gmail.com>
2024-11-20 17:01:43 +0900

    path修正

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-export default defineConfig({
-  root: "./src",
-  base: "./",
-  build: {
-    outDir: '../docs',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        about: resolve(__dirname, 'src/about/index.html'),
-        news: resolve(__dirname, 'src/news/index.html')
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? './' : '/barba-template/'
+
+  return {
+    root: "./src",
+    base: base,
+    build: {
+      outDir: '../docs',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          about: resolve(__dirname, 'src/about/index.html'),
+          news: resolve(__dirname, 'src/news/index.html')
+        }
       }
     }
   }
