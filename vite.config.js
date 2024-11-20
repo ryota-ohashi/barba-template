b4cc361e00c82b4349ec951a6ca7952e2af6109d
ryota ohashi <ryota.812.skt@gmail.com>
2024-11-20 16:39:29 +0900

    設定修正

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: "./src",
   base: "./",
   build: {
     outDir: '../docs',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/about/index.html'),
+        news: resolve(__dirname, 'src/news/index.html')
+      }
+    }
   }
 })
